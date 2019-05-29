0782c0f0f1ae672c95a5da5f4bed9083a8f8250d
shawroad <865399807@qq.com>
2019-05-29 20:59:06 +0800

    add position vector

---
+++ Text classification/005- keras之transformer进行文本分类.py
@@ -0,0 +1,152 @@
+"""
+
+@file   : keras之transformer.py
+
+@author : xiaolu
+
+@time1  : 2019-05-27
+
+"""
+from keras.preprocessing import sequence
+from keras.datasets import imdb
+from keras.layers import *
+from keras.models import Model
+from keras import backend as K
+from keras.engine.topology import Layer
+
+
+class Position_Embedding(Layer):
+
+    # 位置向量  # mode 提供了，将位置向量和原向量组合的方式  有相加和拼接
+    def __init__(self, size=None, mode='sum', **kwargs):
+        self.size = size  # 必须为偶数
+        self.mode = mode
+        super(Position_Embedding, self).__init__(**kwargs)
+
+    def call(self, x):
+        # 直接构造绝对位置向量
+        if (self.size == None) or (self.mode == 'sum'):
+            self.size = int(x.shape[-1])
+        batch_size, seq_len = K.shape(x)[0], K.shape(x)[1]
+        # 通过公式算位置向量           K.arange(self.size / 2, dtype='float32') 相当于位置向量
+        position_j = 1. / K.pow(10000., 2 * K.arange(self.size / 2, dtype='float32') / self.size)
+        position_j = K.expand_dims(position_j, 0)
+        position_i = K.cumsum(K.ones_like(x[:, :, 0]), 1) - 1  # K.arange不支持变长，只好用这种方法生成
+        position_i = K.expand_dims(position_i, 2)
+        position_ij = K.dot(position_i, position_j)
+        position_ij = K.concatenate([K.cos(position_ij), K.sin(position_ij)], 2)
+        if self.mode == 'sum':
+            return position_ij + x
+        elif self.mode == 'concat':
+            return K.concatenate([position_ij, x], 2)
+
+    def compute_output_shape(self, input_shape):
+        if self.mode == 'sum':
+            return input_shape
+        elif self.mode == 'concat':
+            return (input_shape[0], input_shape[1], input_shape[2] + self.size)
+
+
+class Attention(Layer):
+
+    def __init__(self, nb_head, size_per_head, mask_right=False, **kwargs):
+        self.nb_head = nb_head
+        self.size_per_head = size_per_head
+        self.output_dim = nb_head * size_per_head
+        self.mask_right = mask_right
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # 初始化三个线性变化矩阵
+        self.WQ = self.add_weight(name='WQ',
+                                  shape=(input_shape[0][-1], self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        self.WK = self.add_weight(name='WK',
+                                  shape=(input_shape[1][-1], self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        self.WV = self.add_weight(name='WV',
+                                  shape=(input_shape[2][-1], self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        super(Attention, self).build(input_shape)
+
+    def Mask(self, inputs, seq_len, mode='mul'):
+        if seq_len == None:
+            return inputs
+        else:
+            mask = K.one_hot(seq_len[:, 0], K.shape(inputs)[1])
+            mask = 1 - K.cumsum(mask, 1)
+            for _ in range(len(inputs.shape) - 2):
+                mask = K.expand_dims(mask, 2)
+            if mode == 'mul':
+                return inputs * mask
+            if mode == 'add':
+                return inputs - (1 - mask) * 1e12
+
+    def call(self, x):
+        global Q_seq, K_seq, V_seq, V_len, Q_len
+        # 如果只传入Q_seq,K_seq,V_seq，那么就不做Mask
+        # 如果同时传入Q_seq,K_seq,V_seq,Q_len,V_len，那么对多余部分做Mask
+        if len(x) == 3:
+            Q_seq, K_seq, V_seq = x
+            Q_len, V_len = None, None
+        elif len(x) == 5:
+            Q_seq, K_seq, V_seq, Q_len, V_len = x
+        # 将真正的输入做Q、K、V线性变换  得到真正的q, k, v
+        Q_seq = K.dot(Q_seq, self.WQ)
+        Q_seq = K.reshape(Q_seq, (-1, K.shape(Q_seq)[1], self.nb_head, self.size_per_head))
+        Q_seq = K.permute_dimensions(Q_seq, (0, 2, 1, 3))
+        K_seq = K.dot(K_seq, self.WK)
+        K_seq = K.reshape(K_seq, (-1, K.shape(K_seq)[1], self.nb_head, self.size_per_head))
+        K_seq = K.permute_dimensions(K_seq, (0, 2, 1, 3))
+        V_seq = K.dot(V_seq, self.WV)
+        V_seq = K.reshape(V_seq, (-1, K.shape(V_seq)[1], self.nb_head, self.size_per_head))
+        V_seq = K.permute_dimensions(V_seq, (0, 2, 1, 3))
+        # 计算内积，然后mask，然后softmax
+        A = K.batch_dot(Q_seq, K_seq, axes=[3, 3]) / self.size_per_head ** 0.5
+        A = K.permute_dimensions(A, (0, 3, 2, 1))
+        A = self.Mask(A, V_len, 'add')
+        A = K.permute_dimensions(A, (0, 3, 2, 1))
+        if self.mask_right:
+            ones = K.ones_like(A[:1, :1])
+            mask = (ones - K.tf.matrix_band_part(ones, -1, 0)) * 1e12
+            A = A - mask
+        A = K.softmax(A)
+        # 输出并mask
+        O_seq = K.batch_dot(A, V_seq, axes=[3, 2])
+        O_seq = K.permute_dimensions(O_seq, (0, 2, 1, 3))
+        O_seq = K.reshape(O_seq, (-1, K.shape(O_seq)[1], self.output_dim))
+        O_seq = self.Mask(O_seq, Q_len, 'mul')
+        return O_seq
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], input_shape[0][1], self.output_dim)
+
+
+# 加载数据 建立模型
+max_feature = 20000   # 只取常用的20000个词
+maxlen = 80  # 每个文本中前80个词
+batch_size = 32
+
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_feature)
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+S_inputs = Input(shape=(None,), dtype='int32')
+embeddings = Embedding(max_feature, 128)(S_inputs)  # 首先将所以词进行Embedding
+embeddings = Position_Embedding()(embeddings)   # 然后在进行位置向量
+O_seq = Attention(8, 16)([embeddings, embeddings, embeddings])    #
+O_seq = GlobalAveragePooling1D()(O_seq)
+O_seq = Dropout(0.5)(O_seq)
+outputs = Dense(1, activation='sigmoid')(O_seq)
+
+model = Model(inputs=S_inputs, outputs=outputs)
+# try using different optimizers and different optimizer configs
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print('Train...')
+model.fit(x_train, y_train, batch_size=batch_size, epochs=5, validation_data=(x_test, y_test))
+
